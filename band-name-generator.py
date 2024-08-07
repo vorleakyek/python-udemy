84e8fa0500e470af2e683bc7641a91d5727ed8c0
Vorleak Yek <vorleakyek@gmail.com>
2024-08-06 19:12:21 -0700

    Create band-name-generator.py

---
+++ band-name-generator.py
@@ -0,0 +1,6 @@
+//Band name generator project 
+
+print("Welcome to the band name generator: ")
+city_name = input("What city did you grew up in?\n")
+pet_name = input("What was your pet name?\n")
+print("band name: " + city_name + " " + pet_name)
