42945c5500b9cab0cca70b760eb9456af989492e
Vorleak Yek <vorleakyek@gmail.com>
2024-08-25 23:58:31 -0700

    Adding a Snake class

---
+++ snake_game/main.py
@@ -0,0 +1,24 @@
+import time
+from turtle import Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+screen.exitonclick()
+
+
+
+
